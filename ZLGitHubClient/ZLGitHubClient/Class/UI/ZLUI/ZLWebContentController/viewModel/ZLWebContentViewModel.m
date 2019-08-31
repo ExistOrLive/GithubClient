014b26f5150ee0082a54342a35b7afe6f804741d
朱猛 <18351927991@163.com>
2019-09-01 02:24:40 +0800

    ZLWebContentController 进度条，标题，支持HTTP

--- ZLGitHubClient/ZLGitHubClient/Class/UI/ZLUI/ZLWebContentController/viewModel/ZLWebContentViewModel.m
+++ ZLGitHubClient/ZLGitHubClient/Class/UI/ZLUI/ZLWebContentController/viewModel/ZLWebContentViewModel.m
@@ -30,8 +30,6 @@
     }
     
     self.webContentView = (ZLWebContentView *)targetView;
-    self.webContentView.webView.UIDelegate = self;
-    self.webContentView.webView.navigationDelegate = self;
     
     self.url = (NSURL *) targetModel;
     
@@ -50,91 +48,6 @@
     [self.viewController.navigationController popViewControllerAnimated:YES];
 }
 
-#pragma mark - WKUIDelegate,WKNavigationDelegate
 
-- (nullable WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures
-{
-    return webView;
-}
-
-- (void)webViewDidClose:(WKWebView *)webView
-{
-    
-}
-- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler
-{
-    
-}
-
-- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler
-{
-    
-}
-
-- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * _Nullable result))completionHandler
-{
-    
-}
-
-
-
-- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
-{
-    decisionHandler(WKNavigationActionPolicyAllow);
-}
-
-// Decides whether to allow or cancel a navigation after its response is known.
-- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
-{
-    decisionHandler(WKNavigationResponsePolicyAllow);
-}
-
-//Invoked when a main frame navigation starts.
-- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation
-{
-    
-}
-
-
-- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation
-{
-    
-}
-
-
-- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error
-{
-    
-}
-
-
-- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation
-{
-    
-}
-
-
-- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation
-{
-    
-}
-
-
-- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error
-{
-    
-}
-
-//  @discussion If you do not implement this method, the web view will respond to the authentication challenge with the NSURLSessionAuthChallengeRejectProtectionSpace disposition.
-//- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler
-//{
-//
-//}
-
-
-- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView
-{
-    
-}
 
 @end
