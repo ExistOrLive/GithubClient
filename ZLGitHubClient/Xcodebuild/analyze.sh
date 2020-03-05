fbf2b0ea939dd8ee3988e3aa24e0773ab00ee8fa
朱猛 <2068531506@qq.com>
2020-03-05 11:09:45 +0800

    废弃 OCLint
    Signed-off-by: 朱猛 <2068531506@qq.com>

--- ZLGitHubClient/Xcodebuild/analyze.sh
+++ ZLGitHubClient/Xcodebuild/analyze.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-# 静态分析 脚本
+# oclint 静态分析 脚本
 
-xcodebuild clean 
+# clean build cache
+#xcodebuild clean -workspace ZLGitHubClient.xcworkspace -scheme ZLGitHubClient 
 
-rm -rf Xcodebuild/analyze
+#rm -rf Xcodebuild/analyze
 
-analyzeDirPath="Xcodebuild/analyze/`date +"%Y-%m-%d"`" 
-mkdir -p $analyzeDirPath
+#analyzeDirPath="Xcodebuild/analyze/`date +"%Y-%m-%d"`" 
+#mkdir -p $analyzeDirPath
 
 # xcodebuild analyze 分析工程代码 输出compile_commands.json
-xcodebuild analyze -workspace ZLGitHubClient.xcworkspace -scheme ZLGitHubClient | tee  $analyzeDirPath"/analyze.log" | xcpretty --report json-compilation-database -o $analyzeDirPath'/compile_commands.json'
+#xcodebuild analyze -workspace ZLGitHubClient.xcworkspace -scheme ZLGitHubClient | tee  $analyzeDirPath"/analyze.log" | xcpretty --report json-compilation-database -o $analyzeDirPath'/compile_commands.json'
 
 # oclint-json-compilation-database 分析 compile_commands.json 输出报告
-oclint-json-compilation-database  -p $analyzeDirPath -- -report-type pmd -o $analyzeDirPath'/report.html'
+#oclint-json-compilation-database -p $analyzeDirPath -e Pods -v --  -report-type=html -o=$analyzeDirPath'/report.html' 
 
 # 删除中间文件
 #rm $analyzeDirPath'/compile_commands.json'
+
+## one issue can not be fixed 
+
+# oclint: Not enough positional command line arguments specified!
+#Must specify at least 1 positional argument: See: /usr/local/bin/oclint -help
+
+
+## infer 静态分析脚本
+
