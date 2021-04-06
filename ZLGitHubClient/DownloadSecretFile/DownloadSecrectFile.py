be911af919fc22d3e87771664729bb0d668a9514
朱猛 <2068531506@qq.com>
2021-04-06 22:55:14 +0800

    DownloadSecretFile

--- ZLGitHubClient/DownloadSecretFile/DownloadSecrectFile.py
+++ ZLGitHubClient/DownloadSecretFile/DownloadSecrectFile.py
@@ -3,22 +3,26 @@
 import requests
 import sys
 
-token = sys.argv[1]
+def main():
 
-print(token)
+    token = sys.argv[1]
+    
+    print(token)
+    
+    result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GithubClient/ZLGithubAppKey.h",
+                          headers={"Authorization":"token "+token,"Accept":"application/vnd.github.v3.raw+json"})
+    if result.status_code == 200 :
+       open("ZLGithubAppKey.h",'wb').write(result.content)
+       print("download success")
+    else :
+       print(result)
 
-result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GithubClient/ZLGithubAppKey.h",
+    result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GithubClient/GoogleService-Info.plist",
                       headers={"Authorization":"token "+token,"Accept":"application/vnd.github.v3.raw+json"})
-if result.status_code == 200 :
-    open("ZLGithubAppKey.h",'wb').write(result.content)
-    print("download success")
-else :
-    print(result)
+    if result.status_code == 200 :
+       open("GoogleService-Info.plist",'wb').write(result.content)
+       print("download success")
 
-result = requests.get("https://api.github.com/repos/ExistOrLive/SecretFile/contents/GithubClient/GoogleService-Info.plist",
-                      headers={"Authorization":"token "+token,"Accept":"application/vnd.github.v3.raw+json"})
-if result.status_code == 200 :
-    open("GoogleService-Info.plist",'wb').write(result.content)
-    print("download success")
-
+if __name__ == '__main__':
+    main()
 
