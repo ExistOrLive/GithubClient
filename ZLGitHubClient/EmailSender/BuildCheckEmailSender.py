fb7bdd472843b56c59df2ee4a5a19cd57fb3fae3
朱猛 <2068531506@qq.com>
2021-11-28 01:18:32 +0800

    BuildCheckEmailSender

---
+++ ZLGitHubClient/EmailSender/BuildCheckEmailSender.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+import sys
+import os
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
+sender = 'noreply@existorlive.cn'
+senderPwd = sys.argv[1]
+receivers = sys.argv[2].split(";")
+action_result = sys.argv[3]
+
+env_dist = os.environ
+for key in env_dist:
+    print(key + " : " + env_dist.get(key," "))
+
+# msgRoot = MIMEMultipart('related')
+# msgRoot['From'] = Header("ExistOrLive", 'utf-8')
+# msgRoot['To'] = Header("Dev", 'utf-8')
+
+# if "false" == action_result:
+
+#    subject = 'ZLGithubClient build check action failed'
+#    msgRoot['Subject'] = Header(subject, 'utf-8')
+#    msgAlternative = MIMEMultipart('alternative')
+#    msgRoot.attach(msgAlternative)
+
+#    mail_msg = """
+#    <p>Github Action: ZLGithubClient Auto Buidl failed</p>
+#    <p><a href="https://github.com/MengAndJie/GithubClient/actions">build log</a></p>
+#    """
+#    msgAlternative.attach(MIMEText(mail_msg, 'html', 'utf-8'))
+# else:
+
+#     subject = 'ZLGithubClient build check action Success'
+#     msgRoot['Subject'] = Header(subject, 'utf-8')
+
+#     msgAlternative = MIMEMultipart('alternative')
+#     msgRoot.attach(msgAlternative)
+
+#     mail_msg = """
+#     <p>Github Action: ZLGithubClient Auto Build Success, Please download newest beta version</p>
+#     <p><a href="itms-services://?action=download-manifest&url=https://existorlive.github.io/public/GithubClient/CodingArtifacts/manifest.plist">Beta Version</a></p>
+#     <p><img src="cid:image1"/></p>
+#     """
+#     msgAlternative.attach(MIMEText(mail_msg, 'html', 'utf-8'))
+
+#     # 指定图片为当前目录
+#     fp = open('coding.png', 'rb')
+#     msgImage = MIMEImage(fp.read())
+#     fp.close()
+
+#     # 定义图片 ID，在 HTML 文本中引用
+#     msgImage.add_header('Content-ID', '<image1>')
+#     msgRoot.attach(msgImage)
+
+
+# try:
+#     smtpObj = smtplib.SMTP('smtp.exmail.qq.com')
+#     smtpObj.login(sender,senderPwd)
+#     smtpObj.sendmail(sender, receivers, msgRoot.as_string())
+#     print("邮件发送成功")
+# except smtplib.SMTPException as e:
+#     print(str(e))
+#     print("Error: 无法发送邮件")
