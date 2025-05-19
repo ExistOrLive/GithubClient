e38b0c14576d0fdef896638c3f6af387505a54e2
ExistOrLive <2068531506@qq.com>
2025-05-20 01:46:04 +0800

    【修改信息】 compare

---
+++ ZLGitHubClient/ZLGitHubClient/Class/UI/Commit/ZLCompareCommitController.swift
@@ -0,0 +1,96 @@
+//
+//  ZLRepoCompareCommitController.swift
+//  ZLGitHubClient
+//
+//  Created by 朱猛 on 2025/5/20.
+//  Copyright © 2025 ZM. All rights reserved.
+//
+
+//
+//  ZLRepoCommitController.swift
+//  ZLGitHubClient
+//
+//  Created by 朱猛 on 2020/3/16.
+//  Copyright © 2020 ZM. All rights reserved.
+//
+
+import UIKit
+import ZMMVVM
+import ZLUIUtilities
+import ZLGitRemoteService
+
+class ZLRepoCompareCommitController: ZMTableViewController {
+    
+    @objc var login: String?
+    @objc var repoName: String?
+    @objc var baseRef: String?
+    @objc var headRef: String?
+    
+    var model: ZLGithubCompareModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewStatus = .loading
+        refreshLoadNewData()
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        self.title = ZLLocalizedString(string: "commits", comment: "提交")
+        
+        tableView.contentInsetAdjustmentBehavior = .automatic
+        tableView.contentInset = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
+        setRefreshViews(types: [.header])
+        tableView.register(ZLCommitTableViewCell.self,
+                           forCellReuseIdentifier: "ZLCommitTableViewCell")
+    }
+    
+    override func refreshLoadNewData() {
+        loadData(isLoadNew: true)
+    }
+    
+
+    
+}
+
+
+extension ZLRepoCompareCommitController {
+    func loadData(isLoadNew: Bool) {
+        
+        ZLRepoServiceShared()?.getRepoCommitCompare(withLogin: login ?? "",
+                                                    repoName: repoName ?? "",
+                                                    baseRef: baseRef ?? "",
+                                                    headRef: headRef ?? "",
+                                                    serialNumber: NSString.generateSerialNumber())
+        { [weak self] resultModel in
+            guard let self else { return }
+            if resultModel.result == true, let model = resultModel.data as? ZLGithubCompareModel {
+                self.model = model
+                let cellDataArray: [ZLCommitTableViewCellData] = model.commits.map {
+                    let cellData = ZLCommitTableViewCellData(commitModel: $0)
+                    return cellData
+                }
+                self.zm_addSubViewModels(cellDataArray)
+            
+                self.sectionDataArray.forEach { $0.zm_removeFromSuperViewModel() }
+                self.sectionDataArray = [ZMBaseTableViewSectionData(cellDatas: cellDataArray)]
+               
+                self.tableView.reloadData()
+                
+                self.endRefreshViews(noMoreData: true )
+                self.viewStatus = self.tableViewProxy.isEmpty ? .empty : .normal
+                
+            } else {
+                self.endRefreshViews()
+                self.viewStatus = self.tableViewProxy.isEmpty ? .error : .normal
+                
+                if let errorModel = resultModel.data as? ZLGithubRequestErrorModel {
+                    ZLToastView.showMessage(errorModel.message)
+                }
+            }
+        }
+               
+    }
+    
+}
---
+++ ZLGitHubClient/ZLGitHubClient/Class/UI/Commit/ZLCompareCommitFilesController.swift
@@ -0,0 +1,186 @@
+//
+//  ZLCompareCommitFilesController.swift
+//  ZLGitHubClient
+//
+//  Created by 朱猛 on 2025/5/20.
+//  Copyright © 2025 ZM. All rights reserved.
+//
+
+import UIKit
+import ZLGitRemoteService
+import ZLUIUtilities
+import ZLUtilities
+import ZMMVVM
+
+class ZLCompareCommitFilesController: ZMTableViewController {
+
+    @objc var login: String?
+    @objc var repoName: String?
+    @objc var baseRef: String?
+    @objc var headRef: String?
+    
+    var model: ZLGithubCompareModel?
+    var lastIndex = 0
+    let per_page = 10
+    
+    @objc init() {
+        super.init(style: .plain)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewStatus = .loading
+        refreshLoadNewData()
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        self.title = ZLLocalizedString(string: "commits", comment: "提交")
+        
+
+        tableView.register(ZLCommitInfoPatchCell.self,
+                           forCellReuseIdentifier: "ZLCommitInfoPatchCell")
+        tableView.register(ZLCommitInfoFilePathHeaderView.self,
+                           forHeaderFooterViewReuseIdentifier: "ZLCommitInfoFilePathHeaderView")
+        tableView.register(ZLCommonSectionHeaderFooterView.self,
+                           forHeaderFooterViewReuseIdentifier: "ZLCommonSectionHeaderFooterView")
+        
+        self.setRefreshViews(types: [.header,.footer])
+    }
+    
+    override func refreshLoadNewData() {
+        requestCompareInfo()
+    }
+    
+    override func refreshLoadMoreData() {
+        generateMoreFileData()
+    }
+  
+}
+
+
+
+
+// MARK: - Request
+extension ZLCompareCommitFilesController {
+    
+    func requestCompareInfo() {
+
+            ZLRepoServiceShared()?.getRepoCommitCompare(withLogin: login ?? "",
+                                                        repoName: repoName ?? "",
+                                                        baseRef: baseRef ?? "",
+                                                        headRef: headRef ?? "",
+                                                        serialNumber: NSString.generateSerialNumber())
+            { [weak self] resultModel in
+                guard let self else { return }
+                if resultModel.result == true,
+                   let model = resultModel.data as? ZLGithubCompareModel {
+                
+                self.model = model
+                self.lastIndex = 0
+                
+                let sectionDatas = self.generateFirstPageSectionDatas(data: model)
+                
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.milliseconds(200), execute: {
+                    
+                    self.sectionDataArray.forEach { $0.zm_removeFromSuperViewModel() }
+                    
+                    sectionDatas.forEach { $0.zm_addSuperViewModel(self) }
+               
+                
+                    self.sectionDataArray = sectionDatas
+                    
+                    self.tableView.reloadData()
+                    self.endRefreshViews(noMoreData: model.files.count <= self.lastIndex)
+                    self.viewStatus = self.tableViewProxy.isEmpty ? .empty : .normal
+                })
+                
+            } else {
+                if let errorModel = resultModel.data as? ZLGithubRequestErrorModel {
+                    ZLToastView.showMessage(errorModel.message)
+                }
+                self.endRefreshViews()
+                self.viewStatus = self.tableViewProxy.isEmpty ? .error : .normal
+            }
+        }
+    }
+    
+    func generateMoreFileData() {
+        let sectionDatas = self.generateNextPageFileData()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.milliseconds(200), execute: {
+            
+      
+            sectionDatas.forEach { $0.zm_addSuperViewModel(self) }
+       
+        
+            self.sectionDataArray.append(contentsOf: sectionDatas)
+            
+            self.tableView.reloadData()
+            
+            self.endRefreshViews(noMoreData: (self.model?.files.count ?? 0) <= self.lastIndex)
+        })
+    }
+
+}
+
+// MARK: - generate Data
+extension ZLCompareCommitFilesController {
+    
+    @objc dynamic func generateFirstPageSectionDatas(data: ZLGithubCompareModel) -> [ZMBaseTableViewSectionData] {
+        var sectionDatas: [ZMBaseTableViewSectionData] = []
+
+
+        var files = data.files
+        if files.count > per_page {
+            files = Array(files[0..<per_page])
+            self.lastIndex = per_page
+        } else {
+            self.lastIndex = files.count
+        }
+        
+        
+        let fileSectionDatas = files.map({ fileModel in
+            let sectionData = ZMBaseTableViewSectionData()
+            sectionData.headerData = ZLCommitInfoFilePathHeaderViewData(filePath:fileModel.filename)
+            sectionData.cellDatas = [ZLCommitInfoPatchCellData(model: fileModel,
+                                                               cellHeight: nil)]
+            return sectionData
+        })
+        sectionDatas.append(contentsOf: fileSectionDatas)
+    
+        return sectionDatas
+    }
+    
+    @objc dynamic func generateNextPageFileData() -> [ZMBaseTableViewSectionData] {
+        guard let model else { return [] }
+        
+        var files = model.files
+        let newLastIndex = self.lastIndex + self.per_page
+        if files.count > newLastIndex {
+            files = Array(files[self.lastIndex..<newLastIndex])
+            self.lastIndex = newLastIndex
+        } else {
+            files = Array(files[self.lastIndex..<files.count])
+            self.lastIndex = model.files.count
+        }
+        
+        let fileSectionDatas = files.map({ fileModel in
+            let sectionData = ZMBaseTableViewSectionData()
+            sectionData.headerData = ZLCommitInfoFilePathHeaderViewData(filePath:fileModel.filename)
+            sectionData.cellDatas = [ZLCommitInfoPatchCellData(model: fileModel,
+                                                               cellHeight: nil)]
+            return sectionData
+        })
+    
+        return fileSectionDatas
+    }
+}
+
+
+
